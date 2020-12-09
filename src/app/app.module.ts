16555deb15cd73c3bc4b5d59050a98ed7485c94a
tjnk24 <tjnk24@gmail.com>
2020-12-09 16:29:40 +0300

    implementing global styles, building up organizer without functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { OrganizerComponent } from './components/organizer/organizer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrganizerComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/organizer/organizer.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-organizer',
+  templateUrl: './organizer.component.html',
+  styleUrls: ['./organizer.component.scss']
+})
+export class OrganizerComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
